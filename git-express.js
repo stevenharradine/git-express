e4ee311506fea83e02455f6cd14c931ade6f505e
steven harradine <steven.harradine@telus.com>
2015-07-10 18:12:57 -0400

    output last commit message

--- git-express.js
+++ git-express.js
@@ -1,8 +1,29 @@
 var express = require('express')
+var Git = require("nodegit")
 var app = express()
 
+/*Git.Clone("https://github.com/stevenharradine/git-express.git", "docroot").then(function(repository) {
+  // Work with the repository object here.
+});
+*/
+
+var getMostRecentCommit = function(repository) {
+  return repository.getBranchCommit("master");
+};
+
+var getCommitMessage = function(commit) {
+  return commit.message();
+};
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  
+
+  Git.Repository.open("docroot")
+    .then(getMostRecentCommit)
+    .then(getCommitMessage)
+    .then(function(message) {
+      res.send(message)
+    });
 })
 
 app.listen(3000)
