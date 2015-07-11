b28e55dfaae9fa79b95f86e8785142edfcc05d3a
Steven Harradine <stevenharradine@gmail.com>
2015-07-11 03:29:31 -0400

    fix docroot, add origin to branch call

--- git-express.js
+++ git-express.js
@@ -8,16 +8,20 @@ proxy.use (function (req, res) {
   var commit_hash = url_folders[1]
   var path = url_folders.slice(2).join('/')
 
-  console.log (path)
+  console.log ()
 
-  open(CONFIG.DOC_ROOT)
+  open(CONFIG.DOCROOT)
     // Look up this known commit.
     .then(function(repo) {
+      process.stdout.write("Commit request: " + commit_hash + " ... ")
       // Use a known commit sha from this repository.
       return repo.getCommit(commit_hash);
     })
     // Look up a specific file within that commit.
     .then(function(commit) {
+      console.log ("detected")
+      console.log ("Requested path: " + path)
+
       return commit.getEntry(path);
     })
     // Get the blob contents from the file.
@@ -33,36 +37,40 @@ proxy.use (function (req, res) {
       res.send(String(blob));
     })
     .catch(function(err) {
-    	console.log(err)
+    	var branch_name = commit_hash
 
-    	if (commit_hash == "master") {
-		  open("docroot")//, "docroot", null)
-		    // Look up this known commit.
-		    .then(function(repo) {
-		      // Use a known commit sha from this repository.
-		      return repo.getBranchCommit("master");
-		    })
-		    // Look up a specific file within that commit.
-		    .then(function(commit) {
-		      return commit.getEntry(path);
-		    })
-		    // Get the blob contents from the file.
-		    .then(function(entry) {
-		      // Patch the blob to contain a reference to the entry.
-		      return entry.getBlob().then(function(blob) {
-		        blob.entry = entry;
-		        return blob;
-		      });
-		    })
-		    // Display information about the blob.
-		    .then(function(blob) {
-		      res.send(String(blob));
-		    })
-		    .catch(function(err) {
-		    	console.log(err)
-		    })
-    	}
-      console.log(err)
+    	console.log (err)
+
+        open(CONFIG.DOCROOT)
+          // Look up this known commit.
+          .then(function(repo) {
+          	process.stdout.write("Branch request: " + branch_name + " ... ")
+            // Use a known commit sha from this repository.
+            return repo.getReferenceCommit("origin/" + branch_name)
+          })
+          // Look up a specific file within that commit.
+          .then(function(commit) {
+            console.log ("detected")
+            console.log ("Requested path: " + path)
+
+            return commit.getEntry(path)
+          })
+          // Get the blob contents from the file.
+          .then(function(entry) {
+            // Patch the blob to contain a reference to the entry.
+            return entry.getBlob().then(function(blob) {
+              blob.entry = entry;
+              return blob;
+            });
+          })
+          // Display information about the blob.
+          .then(function(blob) {
+            res.send(String(blob));
+          })
+          .catch(function(err) {
+    	      console.log(err)
+    	      res.send (err)
+          })
     })
 })
 
